814893160a2648b65b42e5199009bf6b54843a60
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-12 20:18:00 +0530

    add event listener to capture rotation around wheel

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+//  import and add material ui react components
 import Button from "@material-ui/core/Button";
 import IconButton from '@material-ui/core/IconButton';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
@@ -7,6 +9,9 @@ import PauseIcon from '@material-ui/icons/Pause';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { grey } from '@material-ui/core/colors';
 
+import ZingTouch from 'zingtouch';
+
+//  update material ui themes with ThemeProvider
 const theme = createMuiTheme({
     palette: {
       primary: {
@@ -15,34 +20,52 @@ const theme = createMuiTheme({
     }
 });
 
-const Wheel = () => {
-  return (
-    <ThemeProvider theme={theme}>
-        <div className="wheel">
-            <div className="rotatable">
-                <div>
-                    <Button color="primary" className="menu-button">
-                        MENU
-                    </Button>
+class Wheel extends React.Component {
+    constructor() {
+        super();
+        this.state = { currentAngle: 0 }
+    }
+
+    //  add event listener to capture rotation around wheel
+    componentDidMount() {
+        var { currentAngle } = this.state;
+        const target = document.querySelector('.rotatable');
+        const region = new ZingTouch.Region(target);
+        region.bind(target, 'rotate', function(e) {
+            currentAngle = e.detail.angle;
+            console.log(currentAngle);
+        });
+    }
+
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <div className="wheel">
+                    <div className="rotatable">
+                        <div>
+                            <Button color="primary" className="menu-button">
+                                MENU
+                            </Button>
+                        </div>
+                        <div className="wheel-mid-div">
+                            <IconButton className="prev-button">
+                                <SkipPreviousIcon />
+                            </IconButton>
+                            <div className="enter-button"></div>
+                            <IconButton className="next-IconButton">
+                                <SkipNextIcon />
+                            </IconButton>
+                        </div>
+                        <div>
+                            <IconButton className="play-pause-button">
+                                <PauseIcon />
+                            </IconButton>
+                        </div>
+                    </div>
                 </div>
-                <div className="wheel-mid-div">
-                    <IconButton className="prev-button">
-                        <SkipPreviousIcon />
-                    </IconButton>
-                    <div className="enter-button"></div>
-                    <IconButton className="next-IconButton">
-                        <SkipNextIcon />
-                    </IconButton>
-                </div>
-                <div>
-                    <IconButton className="play-pause-button">
-                        <PauseIcon />
-                    </IconButton>
-                </div>
-            </div>
-        </div>
-    </ThemeProvider>
-  );
+            </ThemeProvider>
+        );
+    }
 };
 
 export default Wheel;
