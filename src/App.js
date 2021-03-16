3d86a86708024b7a8bfa3825d6ce7c4c715506cf
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-16 23:42:39 +0530

    fix bug regarding accessing classList of undefined

--- src/App.js
+++ src/App.js
@@ -23,23 +23,36 @@ class App extends React.Component {
     let { currentListItem } = this.state;
     let counter = 0;
     const target = document.querySelector('.rotatable');
+    target.draggable = false;
     const region = new ZingTouch.Region(target);
     region.bind(target, 'rotate', function(e) {
       counter += e.detail.distanceFromLast;
       console.log(counter);
       if(counter >= 30){
-        document.getElementsByClassName("list-item")[currentListItem].classList.toggle('active');
+        const prevItem = document.getElementsByClassName("list-item")[currentListItem];
+        if(prevItem){
+          prevItem.classList.toggle('active');
+        }
         currentListItem = (currentListItem + 1) % 4;
-        document.getElementsByClassName("list-item")[currentListItem].classList.toggle('active');
+        const currItem = document.getElementsByClassName("list-item")[currentListItem]
+        if(currItem){
+          currItem.classList.toggle('active');
+        }
         counter = 0;
       }
       if(counter <= -30){
-        document.getElementsByClassName("list-item")[currentListItem].classList.toggle('active');
+        const prevItem = document.getElementsByClassName("list-item")[currentListItem];
+        if(prevItem){
+          prevItem.classList.toggle('active');
+        }
         currentListItem = currentListItem - 1;
         if(currentListItem === -1){
           currentListItem = 3;
         }
-        document.getElementsByClassName("list-item")[currentListItem].classList.toggle('active');
+        const currItem = document.getElementsByClassName("list-item")[currentListItem]
+        if(currItem){
+          currItem.classList.toggle('active');
+        }
         counter = 0;
       }
     });
--- src/Settings.js
+++ src/Settings.js
@@ -4,7 +4,7 @@ class Settings extends React.Component {
     render() {
         return (
             <div className="settings-window">
-                <img src="https://www.flaticon.com/svg/vstatic/svg/975/975660.svg?token=exp=1615917080~hmac=b65d17fdb0fe8770b799205df574f18d" />
+                <img src="https://www.flaticon.com/svg/vstatic/svg/975/975660.svg?token=exp=1615917080~hmac=b65d17fdb0fe8770b799205df574f18d" alt="Settings" />
                 <p>Settings</p>
             </div>
         )
