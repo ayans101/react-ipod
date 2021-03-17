1e4f1ab9481a56288385e5909073c7951135671c
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-17 08:55:09 +0530

    update ui of Games component

--- src/Games.js
+++ src/Games.js
@@ -4,7 +4,8 @@ class Games extends React.Component {
     render() {
         return (
             <div className="games-window">
-                Games
+                <img src="https://www.flaticon.com/svg/vstatic/svg/808/808476.svg?token=exp=1615951970~hmac=a125c65d780dbeeaa4a883b1c7f42cac" alt="Games" />
+                <p>Games</p>
             </div>
         )
     }
